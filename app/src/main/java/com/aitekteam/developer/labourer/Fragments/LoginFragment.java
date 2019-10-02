1523638cff3e9611fbefa1c97d4247a127fe4417
Andy Maulana Yusuf <42409599+goingtoprofandy@users.noreply.github.com>
2019-10-02 22:48:46 +0700

    FIX Portal Route Map

--- app/src/main/java/com/aitekteam/developer/labourer/Fragments/LoginFragment.java
+++ app/src/main/java/com/aitekteam/developer/labourer/Fragments/LoginFragment.java
@@ -4,16 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.aitekteam.developer.labourer.Handlers.PagesHandler;
+import com.aitekteam.developer.labourer.PortalActivity;
 import com.aitekteam.developer.labourer.R;
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
     private View view;
+    private TextView page_login_registration;
 
     public static LoginFragment getInstance() {
         return new LoginFragment();
@@ -26,6 +29,27 @@ public class LoginFragment extends Fragment {
         PagesHandler pagesHandler = new PagesHandler(this.view);
         pagesHandler.showPage(R.id.page_login);
 
+        this.page_login_registration = this.view.findViewById(R.id.page_login_registration);
+        this.page_login_registration.setOnClickListener(this);
+
         return this.view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.page_login_registration) {
+            // Go to Registration
+            try {
+                // TO DO Magic
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (((PortalActivity) getActivity()) != null)
+                    ((PortalActivity) getActivity()).setUpPortal(RegistrationFragment.getInstance());
+            }
+        }
+        else {
+            // Do Login
+        }
     }
 }
